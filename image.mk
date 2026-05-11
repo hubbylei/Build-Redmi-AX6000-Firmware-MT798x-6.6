98b2b50b05c4b4e52e873081875bf53af4684355
hubbylei <hubbylei@hubbylei.dynu.net>
2026-05-12 06:52:02 +0800

    Update build_openwrt.yml

--- image.mk
+++ image.mk
@@ -1,12 +1,10 @@
-
-define Device/xiaomi_redmi-router-ax6000
+define Device/xiaomi_redmi-router-ax6000-stock
   DEVICE_VENDOR := Xiaomi
   DEVICE_MODEL := Redmi Router AX6000
-  DEVICE_VARIANT := (custom U-Boot layout)
+  DEVICE_VARIANT := (stock layout)
   DEVICE_DTS := mt7986a-xiaomi-redmi-router-ax6000-stock
   DEVICE_DTS_DIR := ../dts
   DEVICE_PACKAGES := kmod-leds-ws2812b kmod-mt7915e kmod-mt7986-firmware mt7986-wo-firmware
-  KERNEL_LOADADDR := 0x48000000
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
@@ -16,4 +14,4 @@ define Device/xiaomi_redmi-router-ax6000
   IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += xiaomi_redmi-router-ax6000
+TARGET_DEVICES += xiaomi_redmi-router-ax6000-stock
